9ac88342dc98fba496011843ce6bdd2ec54735e5
alepole11 <alessandro.poletti@gmail.com>
2024-09-14 15:19:19 +0200

    Prima versione

---
+++ script.js
@@ -0,0 +1,9 @@
+function CalcolaFattoriale(){
+	numero = document.getElementById("n").value;
+	//console.log("Numero = "+numero);
+	ris = numero;
+	for (var i = numero-1; i >= 1; i--) {
+		ris = ris*i;
+	}
+	document.getElementById("risultato").innerHTML = ris;
+}
